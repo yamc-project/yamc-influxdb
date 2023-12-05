4708bad867909100575bc3a7f90cb3e8f8c6e8b3
Tomas Vitvar <tomas@vitvar.com>
2023-12-05 14:45:54 +0100

    custom time precision

--- yamc_influxdb/influxdb.py
+++ yamc_influxdb/influxdb.py
@@ -11,6 +11,27 @@ from yamc.writers import Writer, HealthCheckException
 
 from yamc.utils import Map, is_number
 
+# time precision constants; the first number is the multiplier to convert the time to seconds, the second number is
+# the multiplier to detect whether the time is in the given precision
+TIME_PRECISION = [{"ns": (10**9, 10**18)}, {"us": (10**6, 10**15)}, {"ms": (10**3, 10**12)}, {"s": (1, 1)}]
+
+
+def _time_precision(epoch_time):
+    magnitude = abs(epoch_time)
+    for tp in TIME_PRECISION:
+        k, v = list(tp.items())[0]
+        if magnitude > v[1]:
+            return k, v
+    return None, None
+
+
+def _time_precision_item(precision):
+    for tp in TIME_PRECISION:
+        k, v = list(tp.items())[0]
+        if k == precision:
+            return v
+    return None
+
 
 class InfluxDBWriter(Writer):
     def __init__(self, config, component_id):
@@ -20,6 +41,9 @@ class InfluxDBWriter(Writer):
         self.user = self.config.value_str("user", default="")
         self.pswd = self.config.value_str("password", default="")
         self.dbname = self.config.value_str("dbname")
+        self.tp_label = self.config.value_str("time_precision", default="ms", regex="^(s|ms|us|ns)$")
+        print(self.tp_label)
+        self.tp = _time_precision_item(self.tp_label)
         self.max_body_size = self.config.value_str("max-body-size", default=20000000)
         self._client = None
 
@@ -61,9 +85,15 @@ class InfluxDBWriter(Writer):
             size = 0
             for item in items:
                 fields, tags = self._create_fields_tags(item.data)
+                _time = item.data.get("time", 0)
+                tp_p, tp_c = _time_precision(_time)
+                if tp_p != self.tp_label:
+                    _time = int((_time / tp_c[0]) * self.tp[0])
+                else:
+                    _time = int(_time)
                 point = Map(
                     measurement=item.data.get("measurement", item.collector_id),
-                    time=int(item.data.get("time", 0)) * 1000000000,
+                    time=_time,
                     fields=fields,
                     tags=tags,
                 )
@@ -75,6 +105,7 @@ class InfluxDBWriter(Writer):
                 if len(point.fields.keys()) == 0:
                     self.log.warn("There are no fields in the data point %s!" % str(point))
                 points.append(point)
+                self.log.debug("Adding the data point %s to the influxdb." % str(point))
                 size += json.dumps(point).__sizeof__()
                 if size > self.max_body_size:
                     self.log.debug(f"Writing the points as the size exceeded {self.max_body_size} bytes.")
